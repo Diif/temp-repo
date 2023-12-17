355fa90dfd1a8209f16fca6c6b9030221a014432
prozella <kiwulik@yandex.ru>
2023-12-17 22:08:53 +0700

    str

---
+++ Str2.cs
@@ -0,0 +1,23 @@
+﻿using Nsu.ColiseumProblem.Contracts.Cards;
+using Nsu.ColiseumProblem.Contracts;
+public class Str2 : ICardPickStrategy
+{
+  public Str2()
+  {
+    
+  }
+  public int Pick(Card[] cards)
+  {
+   var a = 15;
+    while(a-->0)
+    {
+      if (cards[a].Color == CardColor.Black)
+      {
+        return a;
+      }
+    }
+      
+    
+    return 0;
+  }
+}
